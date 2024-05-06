463ef5d3354090f5609bc6a331166d6f49c59f50
venkatganesh <82666955+venkatganesh96@users.noreply.github.com>
2024-05-06 15:57:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-from langchain.chat_models import AzureChatOpenAI
+import os
+from langchain.chat_models import ChatOpenAI
+
+os.environ["OPENAI_API_KEY"] = "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"
+
+llm_azure = ChatOpenAI()
 printf("hello world")
